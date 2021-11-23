d5a75614aba21783b120061313b3bd8d5b085c76
Sathyamano30 <94217503+Sathyamano30@users.noreply.github.com>
2021-11-23 15:08:46 +0530

    Update and rename employee record.h to Employee record.h

---
+++ 3_Implemetation/inc/Employee record.h
@@ -0,0 +1,15 @@
+/**
+ * @file employee record.h
+ * @author Sathyamano
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <stdio.h> ///for input output functions like printf, scanf
+#include <stdlib.h> /// for dynamic memory allocation and other Standard Functions
+#include <conio.h>/// for console input ouput
+#include <windows.h> ///for windows related functions (not important)
+#include <string.h>  ///string operations
